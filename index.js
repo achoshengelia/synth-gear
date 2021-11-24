488782668c3047274bc94d76e1559cbbe472df0a
Achi Shengelia <achoshengelia@gmail.com>
2021-11-24 23:07:38 +0400

    Fixed heroku errors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const path = require('path');
 
 const AppError = require('./utilities/AppError');
